0175830bbdff714b61cd452260072f2b32507fbe
Eduardo Garcia <eg836151@gmail.com>
2023-08-14 17:50:01 -0300

    Adicionada a atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -25,10 +25,10 @@ app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
     try {
-        const movieWithSameTitle = await prisma.movie.findFirst({ where: { title: {equals: title, mode: "insensitive"} } });
+        const movieWithSameTitle = await prisma.movie.findFirst({ where: { title: { equals: title, mode: "insensitive" } } });
 
-        if(movieWithSameTitle) {
-            return res.status(409).send({message: "Já tem um filme com o mesmo nome cadastrado"});
+        if (movieWithSameTitle) {
+            return res.status(409).send({ message: "Já tem um filme com o mesmo nome cadastrado" });
         }
 
         await prisma.movie.create({
@@ -45,6 +45,32 @@ app.post("/movies", async (req, res) => {
     }
 
     res.status(201).send();
+});
+
+app.put("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: { id }
+        });
+
+        if (!movie) {
+            res.status(404).send({ message: "Filme não encontrado" });
+        }
+
+        const data = { ...req.body };
+        data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+
+        await prisma.movie.update({
+            where: { id },
+            data: data
+        });
+    } catch (err) {
+        return res.status(500).send({message: "Falha ao tentar atualizar o registro do filme"});
+    }
+
+    res.status(200).send({ message: "Filme atualizado" });
 });
 
 app.listen(port, () => {
